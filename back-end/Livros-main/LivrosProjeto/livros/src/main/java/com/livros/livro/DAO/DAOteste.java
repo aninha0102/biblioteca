08f59a9be6ec901fb564aed4803fcfce1205af85
aninha0102 <anabeatriz96587@gmail.com>
2024-12-12 12:17:55 -0300

    Delete back-end/Livros-main directory

--- back-end/Livros-main/LivrosProjeto/livros/src/main/java/com/livros/livro/DAO/DAOteste.java
+++
@@ -1,9 +0,0 @@
-package com.livros.livro.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.livros.livro.model.LivroModel;
-
-public interface DAOteste extends CrudRepository<LivroModel, Integer>{
-
-}
--- back-end/Livros-main/LivrosProjeto/livros/src/main/java/com/livros/livro/repository/LivroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.livros.livro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.livros.livro.model.LivroModel;
-
-public interface LivroRepository extends JpaRepository<LivroModel, Integer> {
-
-}
